59987ba52d21d63b6d8a0bba406197a794dfc580
MikePia <lynnpete11@gmail.com>
2021-05-26 21:39:09 -0600

    testing the isRunning Process

--- bizops/tasks.py
+++ bizops/tasks.py
@@ -1,19 +1,22 @@
 from celery import shared_task
 from time import sleep
-from .businessops import BusinessOps
 from quotedb.models.allquotes_candlemodel import AllquotesModel
-from quotedb.scripts.isrunning import is_running
 from quotedb.finnhub.finncandles import FinnCandles
+from quotedb.utils import util
 from .businessops import getStocks
 import os
+
 
 @shared_task
 def sleepy(duration):
     rfile = "writethedamnfile"
-    fn = os.path.join(os.environ.get('RUNDIR'), rfile)
-    with open(fn, 'w') as f:
-        f.write(str(os.getpid()))
-    sleep(duration)
+    util.startRunning(rfile)
+    
+    while True:
+        sleep(15)
+        if not util.isRunningrfile(rfile):
+            print('file has been deleted')
+            break
     return None
 
 
